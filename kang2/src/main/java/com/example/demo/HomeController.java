fa4c7a0746f87a0a707af65248c5e5c53e97d9ae
wert2 <wert2@LAPTOP-JVUJLVLS>
2022-11-16 23:54:28 +0900

    집에서 작업

--- kang2/src/main/java/com/example/demo/HomeController.java
+++ kang2/src/main/java/com/example/demo/HomeController.java
@@ -7,36 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-	@RequestMapping("/jsp")
-	public String home() {
-		System.out.println("되나");
-		return "index";
-	}
-	
-	
-	@RequestMapping("/")
-	public String main() {
-		return "main";
-	}
-	
-	@RequestMapping("/write_move")
-	public String write_move() {
-		return "common/write";
-	}
-	
-	@GetMapping("/test")
+	@GetMapping("/")
 	public String test() {
 		return "test";
 	}
-	
-	@GetMapping("/ppt")
-	public String ppt() {
-		return "ppt";
-	}
-	
-	@GetMapping("/ppt_contant_test")
-	public String ppt_contant_test() {
-		return "ppt_contant/contant_list";
-	}
-	
 }
