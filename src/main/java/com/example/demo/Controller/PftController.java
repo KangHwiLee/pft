c506701e3b0a8af9d2bf12fcf442737d149ce41b
wert2 <rkdgnl12336@gmail.com>
2023-09-19 01:52:33 +0900

    프로젝트 목록 추가

--- src/main/java/com/example/demo/Controller/PftController.java
+++ src/main/java/com/example/demo/Controller/PftController.java
@@ -1,13 +1,15 @@
 package com.example.demo.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class PftController {
     @GetMapping("/pft/{project}")
-    public String project(@PathVariable String project){
-        return "pft/"+project;
+    public String project(@PathVariable String project, Model model){
+        model.addAttribute("pft", project);
+        return "pft";
     }
 }
